7bca99fcc0ceedd96f56e414a5b8d2232d709e84
Reid Herrera <reidherrerabusiness@gmail.com>
2025-01-16 15:00:18 -0600

    worked on generics :)

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,4 @@
 fn main() {
-    let month_days = 1..31;
-    println!("{month_days:?}");
-
-    let month_days = 1..=31;
-    println!("{month_days:?}");
-
-    for number in month_days {
-        println!("{number}")
-    }
-
-    let letters = 'b'..='q';
-
-    for letter in letters {
-        println!("{letter}")
-    }
-
-    let colors = ["Red", "Green", "Yellow"];
-
-    for color in colors {
-        println!("{color} is fuckin cool")
-    }
+    let month_days: std::ops::Range<i8> = 1..31;
+    let letters: std::ops::Range<char> = 'b'..'f';
 }
